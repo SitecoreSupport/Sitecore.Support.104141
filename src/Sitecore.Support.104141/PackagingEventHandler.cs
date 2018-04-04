409de0c23be09d2295203a47292a17b90b18f21a
Chee How Yap <yho@sitecore.net>
2018-04-04 09:57:34 +0800

    Commit Patch

---
+++ src/Sitecore.Support.104141/PackagingEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Sitecore.Configuration;
+
+namespace Sitecore.Support
+{
+  public class PackagingEventHandler
+  {
+    static SettingsSwitcher customSwitcher;
+
+    public void OnPackageInstallSettingSwitcher(object sender, EventArgs e)
+    {
+      var setting = Settings.AllowDuplicateItemNamesOnSameLevel;
+      if (!setting)
+      {
+        customSwitcher = new SettingsSwitcher("AllowDuplicateItemNamesOnSameLevel", true.ToString());
+      }
+    }
+
+    public void OnPackageInstallDisposeSwitcher(object sender, EventArgs e)
+    {
+      customSwitcher?.Dispose();
+      customSwitcher = null;
+    }
+  }
+}
